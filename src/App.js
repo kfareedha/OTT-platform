31d4351744b0a04fff796b2f56e5591ef1a92ac2
kfareedha <kfareedha@gmail.com>
2022-11-18 12:47:12 +0530

    new banners added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-
-import './App.css';
-import React from 'react'
-import Navbar from './components/navbar/navbar' 
-import {originals,actions } from './urls'
-import Banner from './components/Banner/banner' 
-import Rowpost from './components/Rowpost/Rowpost';
+import "./App.css";
+import React from "react";
+import Navbar from "./components/navbar/navbar";
+import { originals, actions, horrors, comedy, trending } from "./urls";
+import Banner from "./components/Banner/banner";
+import Rowpost from "./components/Rowpost/Rowpost";
 function App() {
   return (
     <div className="App">
-      <Navbar/> 
-      <Banner/> 
-       <Rowpost type='tv' url={originals} title='Netflix Originals'/>
-       <Rowpost  type='movie' url={actions} title='Action' isSmall />
+      <Navbar />
+      <Banner />
+      <Rowpost type="tv" url={originals} title="Netflix Originals" />
+      <Rowpost type="week" url={trending} title="Trending Movies" />
+
+      <Rowpost type="movie" url={horrors} title="Horror Movies" isSmall />
+      <Rowpost type="movie" url={comedy} title="Comedy Movies" isSmall />
+      <Rowpost type="movie" url={actions} title="Action Movies" isSmall />
     </div>
   );
 }
